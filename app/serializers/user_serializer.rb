b7cb037f0b8f02e0e47664fc10737d980681b903
curtisgreene <curtis.j.w.greene@gmail.com>
2017-06-14 15:14:23 -0400

    fixing serialization

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,22 @@ class UserSerializer < ActiveModel::Serializer
   attributes :account_id, :id, :username, :followers, :following
 
   has_many :articles
+
+  def followers
+    object.followers.map do |follower|
+      {
+      username: follower.username,
+      id: follower.id
+    }
+    end
+  end
+
+  def following
+    object.following.map do |person|
+      {
+      username: person.username,
+      id: person.id
+    }
+    end
+  end
 end
